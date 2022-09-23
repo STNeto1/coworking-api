10a0443744e52682bc816f23eb63a1be84fabcb7
STNeto1 <netofreitas7052@gmail.com>
2022-09-22 22:32:01 -0300

    delete rooms with building

--- pkg/building/delete_building.go
+++ pkg/building/delete_building.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func (h handler) DeleteBuilding(c *gin.Context) {
@@ -18,6 +19,26 @@ func (h handler) DeleteBuilding(c *gin.Context) {
 		c.AbortWithStatusJSON(http.StatusUnauthorized, exceptions.NotFound("Building not found"))
 		return
 	}
+
+	var rooms []models.Room
+	if err := h.DB.Where("building_id = ?", building.ID).Find(&rooms).Error; err != nil {
+		c.AbortWithStatusJSON(http.StatusUnauthorized, exceptions.InternalServerError("Error while getting rooms"))
+		return
+	}
+
+	h.DB.Transaction(func(tx *gorm.DB) error {
+		if err := tx.Delete(&building).Error; err != nil {
+			return err
+		}
+
+		for i := 0; i < len(rooms); i++ {
+			if err := tx.Delete(&rooms[i]).Error; err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
 
 	if result := h.DB.Delete(&building); result.Error != nil {
 		c.JSON(http.StatusInternalServerError, exceptions.InternalServerError("Internal error"))
